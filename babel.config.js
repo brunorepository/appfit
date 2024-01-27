0f4d46d1af68a42e36ceeb8960832c43be274d5c
Bruno <brunosilvaf22@hotmail.com>
2024-01-27 18:09:06 -0300

    Adiciona hooks do Husky

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,29 @@
+const presets = ['module:metro-react-native-babel-preset']
+const plugins = []
+
+plugins.push(
+	[
+		'module-resolver',
+		{
+			root: ['.'],
+			extensions: ['.js', '.jsx', '.json', '.ts', '.tsx', '.android.js', '.android.tsx', '.ios.js', '.ios.tsx'],
+			alias: {
+				'@': './src',
+				adapters: './src/adapters',
+				assets: './src/assets',
+				components: './src/components',
+				constants: './src/constants',
+				routes: './src/routes',
+				views: './src/views',
+				store: './src/store',
+				services: './src/services',
+			},
+		},
+	],
+	'react-native-reanimated/plugin'
+)
+
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-};
+	presets,
+	plugins,
+}
